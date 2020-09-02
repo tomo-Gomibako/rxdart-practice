39bae0550c59f4b668452ae205afa08febcca719
tomon9086 <tomo.mouse.9086@gmail.com>
2020-09-02 14:39:39 +0900

    transform

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'dart:async';
 
-StreamController controller = StreamController<String>();
+StreamController controller = StreamController<int>();
 
 void main() {
   subscribe();
 
   int count = 0;
   Timer.periodic(Duration(seconds: 1), (timer) {
-    controller.sink.add("added ${++count} time${count > 1 ? "s" : ""}");
+    controller.sink.add(++count);
   });
 }
 
+// StreamTransformer<DataType, ReturnType>
+StreamTransformer<int, String> transformer() {
+  return StreamTransformer<int, String>.fromHandlers(
+    handleData: (data, sink) {
+      // Error handling
+      if (data < 1) {
+        print("unexpected data");
+        return;
+      }
+      // add to stream
+      sink.add("added $data time${data > 1 ? "s" : ""}");
+    },
+  );
+}
+
 void subscribe() {
-  controller.stream.listen((event) {
+  controller.stream.transform(transformer()).listen((event) {
     print("got: ${event.toString()}");
   });
 }

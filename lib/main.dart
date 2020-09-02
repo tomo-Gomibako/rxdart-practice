9cc3d6ac9d04887f6cd0e528b9aea67fc80d9442
tomon9086 <tomo.mouse.9086@gmail.com>
2020-09-02 14:18:01 +0900

    stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
+import 'dart:async';
+
+StreamController controller = StreamController<String>();
+
 void main() {
-  print("Hello, World!");
+  subscribe();
+
+  int count = 0;
+  Timer.periodic(Duration(seconds: 1), (timer) {
+    controller.sink.add("added ${++count} time${count > 1 ? "s" : ""}");
+  });
+}
+
+void subscribe() {
+  controller.stream.listen((event) {
+    print("got: ${event.toString()}");
+  });
 }
